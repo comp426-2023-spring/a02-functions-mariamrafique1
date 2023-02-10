5820a48e9d0bc47caa62393a46348f52d510f3da
Mariam Rafique <mrafique@unc.edu>
2023-02-09 20:49:18 -0500

    lab

--- cli.js
+++ cli.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
-import moment from "moment-timezone";
 import minimist from "minimist";
+import moment from "moment-timezone";
 import fetch from "node-fetch";
 
-const arg = minimist(process.argv.slice(2));
-if(arg.h) {
-	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+const arg = minimist(process.argv.slice(2)); 
+
+
+
+if (arg.h) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
         -h            Show this help message and exit.
         -n, -s        Latitude: N positive; S negative.
         -e, -w        Longitude: E positive; W negative.
@@ -15,40 +18,39 @@ if(arg.h) {
         -j            Echo pretty JSON from open-meteo API and exit.`
     );
   process.exit(0);
-
 }
 
-const timeZone = moment.tz.guess() 
-var longitude = arg.e || arg.w * -1;
+const time_zone = moment.tz.guess()  
 var latitude = arg.n || arg.s * -1;
+var longitude = arg.e || arg.w * -1;
 
-const link = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&timezone=" + timeZone;
-const response = await fetch(link);
+const url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&timezone=" + time_zone;
+const response = await fetch(url);
 const data = await response.json();
 
-
 if (arg.j){
-	console.log(data);
-	process.exit(0);
+  console.log(data);
+  process.exit(0);
 }
 
-const day = arg.d
+const days = arg.d
 
-if (data.daily.precipitation_hours[days] != 0.0) {
-	console.log("You might need your galoshes");
-
-} else {
-	console.log("You will not need your galoshes");
+if(data.daily.precipitation_hours[days] != 0.0) {
+  console.log("You might need your galoshes");
+} 
+else{
+  console.log("You will not need your galoshes");
 }
 
-if (day ==0) {
-	console.log("today");
-} else if (days > 1) {
-	console.log("in" + day "days.")
-} else {
-	console.log("tomorrow");
+if (days == 0) {
+  console.log("today.")
+} 
+else if (days > 1) {
+  console.log("in " + days + " days.")
+} 
+else {
+  console.log("tomorrow.")
 }
-
 
 
 
